6c22e46b3c76f1fab2c44e45cb7968a0bb139196
NyTsanta Andriamiarisoa <nytsanta.andriams@yahoo.fr>
2022-12-15 14:31:55 +0100

    import 'Hello World' maven project ready to build

---
+++ src/main/java/com/nytsanta2038/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.nytsanta2038.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/nytsanta2038/mavenproject/AppTest.java
@@ -0,0 +1,20 @@
+package com.nytsanta2038.mavenproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
